3da37e41d1418339bb9fee0f0d70ab30d8255b11
Bogdan <bo2002277@gmail.com>
2025-01-07 00:56:07 +0300

    v. 0.0.2.2 Parser - Parse basic insert

--- tage/src/main.rs
+++ tage/src/main.rs
@@ -1,72 +1,81 @@
 use nom::branch::alt;
 use nom::bytes::complete::tag_no_case;
-use nom::character::complete::{alphanumeric1, space1};
+use nom::character::complete::{alpha1, alphanumeric1, i32, space1};
+use nom::error::Error;
 use nom::multi::separated_list1;
 use nom::{bytes::complete::tag, IResult};
-use nom::error::Error;
 
 #[derive(Debug, PartialEq)]
-struct Query{
+struct Query {
     select: Option<SelectQuery>,
-    insert_query: Option<InsertQuery>
+    insert_query: Option<InsertQuery>,
 }
 #[derive(Debug, PartialEq)]
 struct SelectQuery {
     table: String,
-    columns: Vec<String>
+    columns: Vec<String>,
 }
 #[derive(Debug, PartialEq)]
 struct InsertQuery {
     table: String,
     columns: Vec<String>,
-    values: Vec<String>
+    values: Vec<Value>,
+}
+
+#[derive(Debug, PartialEq)]
+struct Value {
+    i_value: Option<i32>,
+    s_value: Option<String>,
 }
 
 // cmu database group (YT, Andy Pavlo)- inspiration what can I to solve
+// + Tony saro
 fn main() {
     println!("Hello, world!");
 }
 
 fn parse(input: &str) -> IResult<&str, Query> {
-    alt((
-        parse_action_select,
-        parse_action_insert
-    ))(input)
+    alt((parse_select, parse_insert))(input)
 }
 
-fn parse_action_select(input: &str) -> IResult<&str, Query> {
+fn parse_select(input: &str) -> IResult<&str, Query> {
     let (input, _) = tag("select")(input)?;
     let (input, _) = space1(input)?;
-    let (input, columns) = parse_select_columns(input)?;
+    let (input, columns) = parse_columns(input)?;
     let (input, _) = space1(input)?;
     let (input, _) = tag("from")(input)?;
     let (input, _) = space1(input)?;
     let (input, table) = alphanumeric1(input)?;
     let (input, _) = tag(";")(input)?;
 
-    Ok((input,
+    Ok((
+        input,
         Query {
             select: Some(SelectQuery {
                 table: table.to_string(),
-                columns: columns.iter().map(ToString::to_string).collect()
+                columns: columns.iter().map(ToString::to_string).collect(),
             }),
-            insert_query: None
-        }
+            insert_query: None,
+        },
     ))
 }
 
-fn parse_action_insert(input: &str) -> IResult<&str, Query> {
+fn parse_insert(input: &str) -> IResult<&str, Query> {
     let (input, _) = tag("insert")(input)?;
     let (input, _) = space1(input)?;
     let (input, _) = tag("into")(input)?;
     let (input, _) = space1(input)?;
     let (input, table) = alphanumeric1(input)?;
     let (input, _) = space1(input)?;
-    let (input, columns) = parse_select_columns(input)?;
+    let (input, _) = tag("(")(input)?;
+    let (input, columns) = parse_columns(input)?;
+    let (input, _) = tag(")")(input)?;
     let (input, _) = space1(input)?;
     let (input, _) = tag("values")(input)?;
     let (input, _) = space1(input)?;
-    let (input, values) = parse_select_columns(input)?;
+    let (input, _) = tag("(")(input)?;
+    let (input, values) = parse_values(input)?;
+    let (input, _) = tag(")")(input)?;
     let (input, _) = tag(";")(input)?;
 
     Ok((
@@ -76,54 +85,48 @@ fn parse_action_insert(input: &str) -> IResult<&str, Query> {
             insert_query: Some(InsertQuery {
                 table: table.to_string(),
                 columns: columns.iter().map(ToString::to_string).collect(),
-                values: values.iter().map(ToString::to_string).collect()
-            })
-        }
+                values,
+            }),
+        },
     ))
 }
 
-fn parse_select(input: &str) -> IResult<&str, SelectQuery> {
-    let (remainder, parsed) = tag("select")(input)?;
-    Ok((remainder, SelectQuery { table: "".to_string(), columns: vec![] }))
-}
-
-
-
-fn parse_select_columns(input: &str) -> IResult<&str, Vec<&str>, Error<&str>> {
-
+fn parse_columns(input: &str) -> IResult<&str, Vec<&str>> {
     separated_list1(tag(", "), alphanumeric1)(input)
 }
 
-
-fn parse_action(input: &str) -> IResult<&str, &str> {
-    alt((
-        tag_no_case("select"),
-        tag_no_case("insert"),
-        tag_no_case("update"),
-        tag_no_case("delete"),
-        tag_no_case("create"),
-        tag_no_case("drop")
-    ))(input)
+fn parse_values(input: &str) -> IResult<&str, Vec<Value>> {
+    separated_list1(tag(", "), parse_value)(input)
 }
 
-fn parse_nothing(input: &str) -> IResult<&str, &str> {
-    tag("select")(input)
-}
-
-fn parse_cols_with_nums(input: &str) -> IResult<&str, &str> {
-    tag("select")(input)
+fn parse_value(input: &str) -> IResult<&str, Value> {
+    let res = i32(input);
+    if res.is_ok() {
+        return res.map(|(input, i_value)| {
+            (
+                input,
+                Value {
+                    i_value: Some(i_value),
+                    s_value: None,
+                },
+            )
+        });
+    }
+    let (input, _) = tag("'")(input)?;
+    let (input, s) = alphanumeric1(input)?;
+    let (input, _) = tag("'")(input)?;
+    Ok((
+        input,
+        Value {
+            i_value: None,
+            s_value: Some(s.to_string()),
+        },
+    ))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_parse_nothing() {
-        let (left, output) = parse_nothing("select * from table").unwrap();
-        assert_eq!(left, " * from table");
-        assert_eq!(output, "select");
-    }
 
     #[test]
     fn test_parse_select() {
@@ -135,46 +138,26 @@ mod tests {
         assert_eq!(query.columns, vec!["col1", "col2"]);
     }
 
+    #[test]
+    fn test_parse_insert() {
+        let (left, output) = parse("insert into table1 (col1, col2) values (1, 'value');").unwrap();
 
-    // select * from t1;
-    #[test]
-    fn test_parse_action() {
-        let (left, output) = parse_action("SELECT * from table").unwrap();
-        assert_eq!(left, " * from table");
-        // assert_eq!(output, "select");
-    }
-
-    #[test]
-    fn test_parse_action_insert() {
-        let (left, output) = parse_action("insert INTO table").unwrap();
-        assert_eq!(left, " INTO table");
-        assert_eq!(output, "insert");
-    }
-    #[test]
-    fn test_parse_action_update() {
-        let (left, output) = parse_action("update table").unwrap();
-        assert_eq!(left, " table");
-        assert_eq!(output, "update");
-    }
-
-    #[test]
-    fn test_parse_action_delete() {
-        let (left, output) = parse_action("delete from table").unwrap();
-        assert_eq!(left, " from table");
-        assert_eq!(output, "delete");
-    }
-
-    #[test]
-    fn test_parse_action_create() {
-        let (left, output) = parse_action("create table").unwrap();
-        assert_eq!(left, " table");
-        assert_eq!(output, "create");
-    }
-
-    #[test]
-    fn test_parse_action_select() {
-        let (left, output) = parse_action("select * from t1").unwrap();
-        assert_eq!(left, " * from t1");
-        assert_eq!(output, "select");
+        assert_eq!(left, "");
+        let query = output.insert_query.unwrap();
+        assert_eq!(query.table, "table1");
+        assert_eq!(query.columns, vec!["col1", "col2"]);
+        assert_eq!(
+            query.values,
+            vec![
+                Value {
+                    i_value: Some(1),
+                    s_value: None
+                },
+                Value {
+                    i_value: None,
+                    s_value: Some("value".to_string())
+                }
+            ]
+        );
     }
 }
